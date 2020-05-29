8919460a1d828fe6ce5e4e52749d7987dbfd4a25
Rashid Jama <rashidjama5@gmail.com>
2020-05-29 01:00:44 -0700

    final commit of hover in and out assignment

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 $("document").ready(function() {
+function dynamic() {
+  return Math.floor(Math.random() * 4);
+}
+
+let dyNum = dynamic();
+console.log(dyNum)
+
   $("img").hover(function() {
    $(this).attr("src")
   },
   function() {
-   $(this).attr("src", "./image/dress5.jpg");
+   $(this).attr("src", "./image/dress"+ dyNum + ".jpg");
   })
 })
